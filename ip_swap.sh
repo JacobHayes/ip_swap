2f6fc0c305faaa9456781f98d3ca2f18583e7563
JacobHayes <jacob.r.hayes@gmail.com>
2013-04-09 23:12:45 -0500

    Modified ip_swap.sh to support call flags instead of just prompts. -d for DHCP, -p for prompt, and -s for static IP.

--- ip_swap.sh
+++ ip_swap.sh
@@ -1,47 +1,109 @@
-function check_connection
+function CHECK_CONNECTION
 {
-   ping -c 1 -t 60  www.google.com > /dev/null
+   ping -c 1 -t 60  www.google.com &> /dev/null
+
+   echo ""
 
    if (( `echo $?` == 0 ))
    then
-      echo ""
       echo "Connected to the internet..."
-
-      connected="y"
    else
-      connected="n"
+      echo "Not connected to the internet..."
    fi
 }
 
-echo "Would you like to connect to DHCP or run with a static ip?"
-echo "NOTE: Swapping may change the connected IP, so ssh clients will be dropped!"
-
-check_connection
-
-echo ""
-echo "D=DHCP    S=static"
-printf "[D]/S: "
-read reply
-reply=${reply:-D}
-
-cur_ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-echo ""
-echo "Current: IPADDR=\"$cur_ip\""
-
-if [[ $reply == "D" || $reply == "d"  ]]
-then
+function DHCP
+{
    cp ifcfg-eth0_dhcp /etc/sysconfig/network-scripts/ifcfg-eth0
 
    echo "New IP is unknown. Check you DHCP server or try ip_tester.sh to check for hosts."
-elif [[ $reply == "S" || $reply == "s"  ]]
-then
+}
+
+function PROMPT
+{
+   echo "Would you like to connect to DHCP or run with a static ip?"
+   echo "NOTE: Swapping may change the connected IP, thus dropping SSH clients!"
+
+   CHECK_CONNECTION
+
+   echo ""
+   echo "D=DHCP    S=static"
+   printf "[D]/S: "
+   read reply
+   reply=${reply:-D}
+
+   cur_ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+   echo ""
+   echo "Current: IPADDR=\"$cur_ip\""
+
+   if [[ $reply == "D" || $reply == "d"  ]]
+   then
+      DHCP
+   elif [[ $reply == "S" || $reply == "s"  ]]
+   then
+      STATIC 
+   else
+      echo "Unknown input. Rerun."
+      exit 1
+   fi
+}
+
+function STATIC
+{
    cp ifcfg-eth0_static /etc/sysconfig/network-scripts/ifcfg-eth0
 
    new_ip=`cat ifcfg-eth0_static | grep 'IPADDR='`
    echo "New:  $new_ip"
-else
-   echo "Unknown input. Rerun."
+}
+
+function USAGE
+{
+   echo "
+############
+SCRIPT USAGE
+############
+
+Script to swap between DHCP and static IP addresses based on the templates provided
+
+OPTIONS:
+   -d
+      Change to DHCP...
+   -p
+      Run the prompt to choose between DHCP and static IPs
+       - This is also called when used without flags
+   -s
+      Change to a static IP...
+"
+
    exit 1
+}
+
+while getopts ":dps" opt
+do
+   case $opt in
+      d)
+         DHCP
+         flag="d"
+         ;;
+      p)
+         PROMPT
+         flag="p"
+         ;;
+      s)
+         STATIC
+         flag="s"
+         ;;
+      ?)
+         echo ""
+         echo "Invalid option: -$OPTARG" >&2
+         USAGE
+         ;;
+   esac
+done
+
+if [[ -z $flag ]]
+then
+   PROMPT
 fi
 
 service network restart
