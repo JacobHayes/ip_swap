37b24fc37cab3b079746004cc3f35c1294afdcbe
JacobHayes <jacob.r.hayes@gmail.com>
2013-04-25 21:25:01 -0500

    Reduced DHCP and static ifcfg_eth0 to one file, ifcfg_eth0.template. Added ability to use custom IPADDR and NETMASK if not defined in the template.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ ip_swap
 =======
 
 Script to change the network binding of Linux systems by swapping eth0 configs
+
+	If changing to a static IP, it will prompt for new IP and netmask unless you add them to ifcfg_eth0.template with "IPADDR=" and "NETMASK="
+
+OPTIONS:
+   -d
+      Change to DHCP...
+   -p
+      Run the prompt to choose between DHCP and static IPs
+       - This is also called when used without flags
+   -s
+      Change to a static IP...
+       - Unless specified in ifcfg_eth0.template, you will be prompted for a new IPADDR and NETMASK
--- ip_swap.sh
+++ ip_swap.sh
@@ -14,17 +14,24 @@ function CHECK_CONNECTION
 
 function DHCP
 {
-   cp ifcfg-eth0_dhcp /etc/sysconfig/network-scripts/ifcfg-eth0
+   cp ifcfg-eth0.template $ifcfg_file
 
+   echo "BOOTPROTO=\"dhcp\"" >> $ifcfg_file
+
+   echo ""
    echo "New IP is unknown. Check you DHCP server or try ip_tester.sh to check for hosts."
 }
 
 function PROMPT
 {
+   CHECK_CONNECTION
+
+   cur_ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+   echo ""
+   echo "Current: IPADDR=\"$cur_ip\""
+
    echo "Would you like to connect to DHCP or run with a static ip?"
    echo "NOTE: Swapping may change the connected IP, thus dropping SSH clients!"
-
-   CHECK_CONNECTION
 
    echo ""
    echo "D=DHCP    S=static"
@@ -32,16 +39,12 @@ function PROMPT
    read reply
    reply=${reply:-D}
 
-   cur_ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-   echo ""
-   echo "Current: IPADDR=\"$cur_ip\""
-
    if [[ $reply == "D" || $reply == "d"  ]]
    then
       DHCP
    elif [[ $reply == "S" || $reply == "s"  ]]
    then
-      STATIC 
+      STATIC
    else
       echo "Unknown input. Rerun."
       exit 1
@@ -50,10 +53,58 @@ function PROMPT
 
 function STATIC
 {
-   cp ifcfg-eth0_static /etc/sysconfig/network-scripts/ifcfg-eth0
+   echo ""
 
-   new_ip=`cat ifcfg-eth0_static | grep 'IPADDR='`
-   echo "New:  $new_ip"
+   cp ifcfg-eth0.template $ifcfg_file
+
+   new_ip=`cat $ifcfg_file | grep 'IPADDR='`
+   status=`echo $?`
+
+   if (( $status == 0 ))
+   then
+      echo "New: $new_ip"
+   elif (( $status == 1 ))
+   then
+      echo "No IP address specified..."
+      printf "Enter a new IP: "
+      read new_ip
+
+      while [[ $new_ip == "" ]]
+      do
+         echo ""
+         echo "No new IP specified."
+         printf "Enter a new IP: "
+         read new_ip
+      done
+
+      echo "New IP address: $new_ip"
+      echo "IPADDR=\"${new_ip}\"" >> $ifcfg_file
+   fi
+
+   echo ""
+   echo ""
+
+   new_netmask=`cat $ifcfg_file | grep 'NETMASK='`
+   status=`echo $?`
+
+   if (( $status == 0 ))
+   then
+      echo "New: $new_netmask"
+   elif (( $status == 1 ))
+   then
+      echo "No Netmask specified..."
+      printf "Enter a new netmask or 'D' for 255.255.255.0: "
+      read new_netmask
+      new_netmask=${new_netmask:-D}
+
+      if [[ $new_netmask == "D" || $new_netmask == "d" ]]
+      then
+         new_netmask="255.255.255.0"
+      fi
+
+      echo "New Netmask: $new_netmask"
+      echo "NETMASK=\"${new_netmask}\"" >> $ifcfg_file
+   fi
 }
 
 function USAGE
@@ -77,6 +128,8 @@ OPTIONS:
 
    exit 1
 }
+
+ifcfg_file="/etc/sysconfig/network-scripts/ifcfg-eth0"
 
 while getopts ":dps" opt
 do
@@ -106,6 +159,7 @@ then
    PROMPT
 fi
 
+echo ""
 service network restart
 
 echo ""
